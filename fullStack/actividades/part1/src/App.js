bf75244156c5e0b00db35fc840bef6785e646452
gordonchis` <duradovaldse@yahoo.com`>
2023-02-05 22:27:19 -0600

    arregle las actividades de react

---
+++ fullStack/actividades/part1/src/App.js
@@ -0,0 +1,12 @@
+import Mensajes from "./components/Mensajes";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <Mensajes msj="hola" />
+    </div>
+  );
+}
+
+export default App;
---
+++ fullStack/actividades/part1/src/components/Mensajes.js
@@ -0,0 +1,9 @@
+const Mensajes = (props) => {
+  return (
+    <div>
+      <h1>{props.msj}</h1>
+    </div>
+  );
+};
+
+export default Mensajes;
